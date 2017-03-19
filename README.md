8e6bc32ae9ad7421890b419d02b65b1d9c6c5b1b
Ben Munro <benjamin.munro@gmail.com>
2017-03-19 23:59:40 +1100

    Problem 1

---
+++ README.md
@@ -0,0 +1,1 @@
+Learning Elixir with Project Euler
---
+++ euler001.exs
@@ -0,0 +1,17 @@
+defmodule Euler1 do
+  def sum(_a, _b, n) when n == 0 do
+    n
+  end
+
+  def sum(a, b, n) do
+    if rem(n, a) == 0 or rem(n, b) == 0 do
+      n + sum(a, b, n-1)
+    else 
+      sum(a, b, n - 1)
+    end
+  end
+
+end
+
+IO.puts "Less than 10:  #{Euler1.sum(3,5,9)}"
+IO.puts "Less than 1000: #{Euler1.sum(3,5,999)}"
